85920687237dc70b31f363251a69311bce8cfbad
Abhishek Pandey <abhishekpandey0525@gmail.com>
2019-10-07 12:27:46 +0530

    changes occured

--- project/js/main.js
+++ project/js/main.js
@@ -9,7 +9,7 @@ function getWeather(searchQuery) {
 
   $.ajax(url,{success:function(data){
     $(".city").text(data.name);
-    $(".temp").text(data.main.temp);
+    $(".temp").text(data.main.humidity);
   },error: function(error){
     $(".error-message").text("An error occured");
   }})
@@ -20,4 +20,23 @@ function getWeather(searchQuery) {
 function searchWeather() {
   var searchQuery =  $("#search").val();
   getWeather(searchQuery);
+}
+function handleSignIn() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 }
